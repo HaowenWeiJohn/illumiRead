5acfbf4d3d1e3534db2fcbf45f59da915314c782
ApocalyVec <59973823+HaowenWeiJohn@users.noreply.github.com>
2023-09-03 02:07:02 -0400

    attention_patch_shape

--- AOIAugmentation/generate_binary_mask_for_oct_report.py
+++ AOIAugmentation/generate_binary_mask_for_oct_report.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 image_height = 1000
 image_width = 2000
-attention_patch_size = np.array([20,20])
+attention_patch_shape = np.array([20,20])
 
 image_path = 'OCT_Image.png'
 image = cv2.imread(image_path)
@@ -15,7 +15,7 @@ image = cv2.resize(image, (image_width, image_height))
 
 binary_mask = generate_image_binary_mask(image)
 
-attention_grid_mask = generate_attention_grid_mask(binary_mask, attention_patch_shape=attention_patch_size)
+attention_grid_mask = generate_attention_grid_mask(binary_mask, attention_patch_shape=attention_patch_shape)
 
 plt.imshow(attention_grid_mask)
 plt.show()
